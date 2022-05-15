a817e6151af5b74487da0a0f545c6b7f230eb9d6
Roberto Orellana <rorepoid@gmail.com>
2022-05-15 16:22:24 -0500

    Format and lint the code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
+import '../styles/globals.css'
+
+function MyApp ({ Component, pageProps }: AppProps): React.ReactElement {
   return <Component {...pageProps} />
 }
 
